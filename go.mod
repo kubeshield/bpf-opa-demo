a1df47935d5df9218fbad7ad3aa15e3459f50a45
Md Tahsin Rahman <tahsynx@gmail.com>
2019-08-30 13:25:41 +0600

    Populate settings map

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/iovisor/gobpf v0.0.0-20190717135640-14e20c7d794b
 	github.com/pkg/errors v0.8.1
+	k8s.io/klog v0.4.0
 )
 
 replace github.com/iovisor/gobpf => github.com/tahsinrahman/gobpf v0.0.0-20190828103435-4226291af08b
--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"strings"
 	"unsafe"
 
-	"github.com/iovisor/gobpf/pkg/cpuonline"
+	"k8s.io/klog"
 
-	bpf "github.com/iovisor/gobpf/elf"
 	"github.com/pkg/errors"
+	"k8s.io/klog/klogr"
+
+	"github.com/iovisor/gobpf/elf"
+	"github.com/iovisor/gobpf/pkg/cpuonline"
+)
+
+var (
+	logger = klogr.New()
 )
 
 func main() {
+	klog.InitFlags(nil)
+
 	cpuRange, err := cpuonline.Get()
 	if err != nil {
 		panic(err)
@@ -19,59 +28,99 @@ func main() {
 	// 0 indexed, so adding 1
 	noCPU := int(cpuRange[len(cpuRange)-1]) + 1
 
-	err = load_bpf_file(noCPU, "bpf/probe.o")
+	module, err := load_bpf_file(noCPU, "bpf/probe.o")
 	if err != nil {
+		panic(err)
+	}
+
+	err = populateSettingsMap(module)
+	if err != nil {
+		logger.Error(err, "failed to populate settings map")
 		panic(err)
 	}
 }
 
-func load_bpf_file(noCPU int, filepath string) error {
-	m := bpf.NewModule(filepath)
-	err := m.Load(map[string]bpf.SectionParams{
-		"maps/perf_map": bpf.SectionParams{
+func load_bpf_file(noCPU int, filepath string) (*elf.Module, error) {
+	m := elf.NewModule(filepath)
+	err := m.Load(map[string]elf.SectionParams{
+		"maps/perf_map": elf.SectionParams{
 			MapMaxEntries: noCPU,
 		},
-		"maps/frame_scratch_map": bpf.SectionParams{
+		"maps/frame_scratch_map": elf.SectionParams{
 			MapMaxEntries: noCPU,
 		},
-		"maps/tmp_scratch_map": bpf.SectionParams{
+		"maps/tmp_scratch_map": elf.SectionParams{
 			MapMaxEntries: noCPU,
 		},
-		"maps/local_state_map": bpf.SectionParams{
+		"maps/local_state_map": elf.SectionParams{
 			MapMaxEntries: noCPU,
 		},
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	progMap := m.Map("tail_map")
 	if progMap == nil {
-		return errors.New("tail map is nil")
+		return nil, errors.New("tail map is nil")
 	}
 
 	progs := m.RawTracePointPrograms()
 
 	for _, prog := range progs {
+		log := logger.WithValues("name", prog.Name, "fd", prog.Fd())
+
 		if strings.Contains(prog.Name, "filler/") {
 			eventName := prog.Name[len("raw_tracepoint/filler/"):]
 			key, found := lookupFillerID(eventName)
 			if !found {
-				return errors.Errorf("filler id not found for program: %v", prog.Name)
+				log.Info("filler id not found")
+				return nil, errors.New("filler id not found")
 			}
 
 			value := prog.Fd()
 
 			err = m.UpdateElement(progMap, unsafe.Pointer(&key), unsafe.Pointer(&value), 0)
 			if err != nil {
-				return err
+				log.Error(err, "failed to update prog map", "key", key, "value", value)
+				return nil, err
 			}
+
+			log.Info("successfully loaded program to tail_map")
+
 		} else {
 			err = m.AttachRawTracepoint(prog.Name)
 			if err != nil {
-				return err
+				log.Error(err, "failed to attach program", "progname", prog.Name)
+				return nil, err
 			}
 		}
+	}
+
+	return m, nil
+}
+
+func populateSettingsMap(m *elf.Module) error {
+	log := logger.WithName("[popultae-settings-map]")
+
+	type bpfSettings struct {
+		capture_enabled bool
+	}
+
+	settingsMap := m.Map("settings_map")
+	key := 0
+	settings := bpfSettings{}
+	err := m.LookupElement(settingsMap, unsafe.Pointer(&key), unsafe.Pointer(&settings))
+	if err != nil {
+		log.Error(err, "failed to lookup settings map", "key", key, "settings", settings)
+		return err
+	}
+
+	settings.capture_enabled = true
+	err = m.UpdateElement(settingsMap, unsafe.Pointer(&key), unsafe.Pointer(&settings), 0)
+	if err != nil {
+		log.Error(err, "failed to update settings map", "key", key, "settings", settings)
+		return err
 	}
 
 	return nil
